6efb9b4ab9aec5fe021a38bff87997b1df65c50b
Abinayan. R <abinayanramalingam2020@gmail.com>
2026-02-07 20:37:25 +0530

    Fix HTML structure and update title in education.php

--- education.php
+++ education.php
@@ -5,7 +5,7 @@
 
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Education detai21255ls</title>
+  <title>Education details</title>
 </head>
 
 <body>
@@ -53,3 +53,4 @@
   
 
 </html>
+
